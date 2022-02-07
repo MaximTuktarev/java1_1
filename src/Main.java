e9eed5315fce0e236ae15c801f9c2545ef7d5c35
MaximTuktarev <maxim.tuktarev@mail.ru>
2022-02-07 18:54:22 +0400

    init

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Расчет бонусных миль");
+
+        int ticketPrice = 520;
+        int miles = ticketPrice / 20;
+        System.out.println(("Бонусные мили: " + miles));
+    }
+}
+
+
+
+
